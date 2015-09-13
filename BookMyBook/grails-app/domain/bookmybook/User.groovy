6f9b85fa8f31fdaf8a463738829d01681642566e
Raunak <raunak.tibrewal@bankbazaar.com>
2015-09-13 12:12:40 +0530

    Grails App 1. DB changes 2. User model 3. Book Model

---
+++ BookMyBook/grails-app/domain/bookmybook/User.groovy
@@ -0,0 +1,15 @@
+package bookmybook
+
+class User {
+
+    Long id
+    String city
+    String deviceId
+    String name
+    String email
+
+    static hasMany = [books: Book]
+
+    static constraints = {
+    }
+}
