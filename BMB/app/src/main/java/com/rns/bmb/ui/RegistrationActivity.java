bfa3aa99e70c0c956338915e4bbecf472f7d7952
codetogamble <shubham.iitkgp10@gmail.com>
2015-09-13 15:10:22 +0530

    Registration form

--- BMB/app/src/main/java/com/rns/bmb/ui/RegistrationActivity.java
+++ BMB/app/src/main/java/com/rns/bmb/ui/RegistrationActivity.java
@@ -5,37 +5,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.rns.bmb.R;
 
 
-public class RegistrationActivity extends Activity {
+public class RegistrationActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        EditText name,email,city;
+        Button btnRegister;
+
+        name =  (EditText)(findViewById(R.id.name));
+        email = (EditText)(findViewById(R.id.email));
+        city = (EditText)(findViewById(R.id.city));
+        btnRegister = (Button)(findViewById(R.id.btn_register));
+
+        btnRegister.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_registration, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_register:
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
